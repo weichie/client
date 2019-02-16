2686a602b5be2369b6a8d522221bbed70ad6074d
Bob Weichler <bobmasaki@hotmail.com>
2019-02-16 11:00:09 +0100

    Dashboard Layout

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,6 +2,53 @@
    <section class="section">
       <div class="container">
          <h1 class="title">Welcome, {{this.$store.state.authStore.userData.username}}!</h1>
+
+         <div class="columns">
+            <div class="column is-two-thirds">
+               <div class="tabs">
+                  <ul>
+                     <li :class="{'is-active': activeTab === 1}">
+                        <a @click.prevent.stop="switchTab(1)">
+                           <span class="icon is-small">
+                              <i class="fas fa-dollar-sign"></i>
+                           </span>
+                           <span>All</span>
+                        </a>
+                     </li>
+                     <li :class="{'is-active': activeTab === 2}">
+                        <a @click.prevent.stop="switchTab(2)">
+                           <span class="icon is-small">
+                              <i class="far fa-smile"></i>
+                           </span>
+                           <span>Income</span>
+                        </a>
+                     </li>
+                     <li :class="{'is-active': activeTab === 3}">
+                        <a @click.prevent.stop="switchTab(3)">
+                           <span class="icon is-small">
+                              <i class="far fa-frown"></i>
+                           </span>
+                           <span>Expenses</span>
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <div class="column">
+               <nav class="panel">
+                  <p class="panel-heading">To Do's</p>
+  
+                  <div class="panel-block">
+                     <p class="control has-icons-left">
+                        <input class="input is-small" type="text" placeholder="Add Todo">
+                        <span class="icon is-small is-left">
+                           <i class="fas fa-plus"></i>
+                        </span>
+                     </p>
+                  </div>
+               </nav>
+            </div>
+         </div>
       </div>
    </section>   
 </template>
@@ -11,7 +58,12 @@
       name: 'Dashboard',
       data(){
          return{
-
+            activeTab: 1
+         }
+      },
+      methods: {
+         switchTab(tabId){
+            this.activeTab = tabId;
          }
       }
    }
