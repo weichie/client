0f43aa6350a1e23c962c05fc0ae62f16131623af
Bob Weichler <bobmasaki@hotmail.com>
2019-02-19 20:08:25 +0100

    Get Client Info

--- src/components/views/Clients.vue
+++ src/components/views/Clients.vue
@@ -10,16 +10,57 @@
                <span>Add new</span>
             </router-link>
          </h1>
+
+         <table class="table is-striped is-narrow is-hoverable is-fullwidth">
+            <thead>
+               <tr>
+                  <th>Company</th>
+                  <th>BTW Number</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Address</th>
+                  <th>Contact</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr v-for="(client, i) in clients" :key="'client_' + i">
+                  <td>{{ client.companyName }}</td>
+                  <td>{{ client.taxId }}</td>
+                  <td>{{ client.email }}</td>
+                  <td>{{ client.phone }}</td>
+                  <td>{{ client.address + ', ' + client.zip + " " + client.city }}</td>
+                  <td>{{ client.firstname + " " + client.lastname }}</td>
+               </tr>
+            </tbody>
+         </table>
       </div>
    </section>
 </template>
 
 <script>
+   import firebase from 'firebase'
+   import db from '../../firestore'
+
    export default {
       name: 'Clients',
       data(){
          return{
-            
+            clients: []
+         }
+      },
+      mounted(){
+         this.getClientInfo();
+      },
+      methods: {
+         getClientInfo(){
+            db.collection('clients')
+               .get()
+               .then(snapshot => {
+                  snapshot.forEach(doc => {
+                     this.clients.push(doc.data());
+                  });
+               })
+               .catch(err => console.error(err));
          }
       }
    }
